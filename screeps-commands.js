d45f85cc7c61a4acfbe4278a19ac5c5d6cbb226c
ybronhaiasi <ybronhaiasi@gmail.com>
2018-02-06 20:21:08 -0800

    Update screeps-commands.js

--- screeps-commands.js
+++ screeps-commands.js
@@ -10,3 +10,9 @@ Game.screeps['name'].memory.role = 'role'
 //Killit!! 
 Game.creeps['name'].suicide()
 
+//Activate your room controller's safe mode
+Game.spawns['Spawn1'].room.controller.activateSafeMode();
+
+//Create a Tower structure at point X:0 Y:0 (still has to be fueled up with source)
+Game.spawns['Spawn1'].room.createConstructionSite( 0, 0, STRUCTURE_TOWER );
+
